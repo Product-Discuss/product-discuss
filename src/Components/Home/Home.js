0d5f34dbdfc06f413f491b42f794c32fda1beb86
Nikhil Joshi <authornjnikhil@gmail.com>
2021-07-18 23:39:22 +0530

    Front Page Beautified!

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,18 +8,33 @@ const Home = () => {
     const [isLoginState, setLoginState] = useState(1);
 
     return (
-        <div id="container">
-            <span id="catchyPhrase">
-                <h1>Catchy Phrase</h1>
-            </span>
-            <span id="log-in">
-                {isLoginState ? (
-                    <Login setLoginState={setLoginState} />
-                ) : (
-                    <Signin setLoginState={setLoginState} />
-                )}
-            </span>
-        </div>
+      <div className={"container"}>
+        <span className={"catchyPhrase"}>
+          <h1 className={"catchyPhraseH1"}>
+            Discuss <span className={"catchyProduct"}>Product</span>
+          </h1>
+          <h1 className={"catchyPhraseH1"}>
+            Rate <span className={"catchyProduct"}>Product</span>
+          </h1>
+          <h1 className={"catchyPhraseH1"}>
+            Buy <span className={"catchyProduct"}>Product</span>
+          </h1>
+
+          <h3 className={"catchyPhraseH3"}>
+            All your faviourates are at one place now !
+          </h3>
+        </span>
+
+      <hr />
+
+        <span className={"log-in-home"}>
+          {isLoginState ? (
+            <Login setLoginState={setLoginState} />
+          ) : (
+            <Signin setLoginState={setLoginState} />
+          )}
+        </span>
+      </div>
     );
 };
 
--- src/Components/Sign in/Signin.js
+++ src/Components/Sign in/Signin.js
@@ -13,54 +13,56 @@ const Signin = (props) => {
         setPassword("");
     };
     return (
-        <span>
-            <form>
-                <label id="name"> Name</label>
-                <br />
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(event) => {
-                        setName(event.target.value);
-                    }}
-                    required
-                />
-                <br />
-                <label id="email"> Email</label>
-                <br />
-                <input
-                    type="text"
-                    value={email}
-                    onChange={(event) => {
-                        setEmail(event.target.value);
-                    }}
-                    required
-                />
-                <br />
-                <label id="pwd">Password</label>
-                <br />
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(event) => {
-                        setPassword(event.target.value);
-                    }}
-                    required
-                />
-            </form>
+      <div className={"signin"}>
+        <form>
+          <label className={"name"}> Name</label>
+          <br />
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            required
+          />
+          <br />
+          <label className={"email"}> Email</label>
+          <br />
+          <input
+            type="text"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            required
+          />
+          <br />
+          <label className={"pwd"}>Password</label>
+          <br />
+          <input
+            type={"password"}
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            required
+          />
+        </form>
 
-            <button id="sign-up-button">Sign up</button>
-            <button id="log-in-google-button">Log in with Google</button>
-            <button
-                id="log-in-button"
-                onClick={() => {
-                    setLoginState(1);
-                    clearAllInputs();
-                }}
-            >
-                Log in
-            </button>
-        </span>
+        <div className={"buttons-div"}>
+          <button className={"sign-up-button"}>Sign up</button>
+          <button className={"log-in-google-button"}>Sign in with Google</button>
+          <button
+            className={"log-in-button"}
+            onClick={() => {
+              setLoginState(1);
+              clearAllInputs();
+            }}
+          >
+            Click here to Log In!
+          </button>
+        </div>
+      </div>
     );
 };
 
