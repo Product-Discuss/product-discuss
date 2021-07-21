9ef577eb3f7397db95c0c89e3c52ae6b82b2435e
KIM-creator <kamalpathak1316@gmail.com>
2021-07-21 15:42:01 +0530

    Google Authentication

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,33 +8,33 @@ const Home = () => {
     const [isLoginState, setLoginState] = useState(1);
 
     return (
-      <div className={"container"}>
-        <span className={"catchyPhrase"}>
-          <h1 className={"catchyPhraseH1"}>
-            Discuss <span className={"catchyProduct"}>Product</span>
-          </h1>
-          <h1 className={"catchyPhraseH1"}>
-            Rate <span className={"catchyProduct"}>Product</span>
-          </h1>
-          <h1 className={"catchyPhraseH1"}>
-            Buy <span className={"catchyProduct"}>Product</span>
-          </h1>
+        <div className={"container"}>
+            <span className={"catchyPhrase"}>
+                <h1 className={"catchyPhraseH1"}>
+                    Discuss <span className={"catchyProduct"}>Product</span>
+                </h1>
+                <h1 className={"catchyPhraseH1"}>
+                    Rate <span className={"catchyProduct"}>Product</span>
+                </h1>
+                <h1 className={"catchyPhraseH1"}>
+                    Buy <span className={"catchyProduct"}>Product</span>
+                </h1>
 
-          <h3 className={"catchyPhraseH3"}>
-            All your faviourates are at one place now !
-          </h3>
-        </span>
+                <h3 className={"catchyPhraseH3"}>
+                    All your favourites are at one place now !
+                </h3>
+            </span>
 
-      <hr />
+            <hr />
 
-        <span className={"log-in-home"}>
-          {isLoginState ? (
-            <Login setLoginState={setLoginState} />
-          ) : (
-            <Signin setLoginState={setLoginState} />
-          )}
-        </span>
-      </div>
+            <span className={"log-in-home"}>
+                {isLoginState ? (
+                    <Login setLoginState={setLoginState} />
+                ) : (
+                    <Signin setLoginState={setLoginState} />
+                )}
+            </span>
+        </div>
     );
 };
 
